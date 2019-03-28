c274158ea495c605670750316209f5a758b25e62
Quinton Aiken <quintonlaiken@gmail.com>
2019-03-28 12:02:32 -0700

    add e2e tests

--- server/server.js
+++ server/server.js
@@ -9,9 +9,7 @@ app.use(express.logger('short'));
 // Serve static files
 app.use(express.static(path.join(__dirname, '../client')));
 
-// Normally, we would trigger a request to the database for our data
-// but we're going to fake that for the sake of this sprint
-var todos = [{ name: 'Client-side unit tests',  done: true},{  name: 'End-to-end workflow tests',  done: true},{  name: 'Visual tests',  done: true},{  name: 'Continous integration',  done: true},{  name: 'Code coverage reporting',  done: true},{  name: 'Server-side unit tests',  done: false},{  name: 'Client-server integration tests',  done: false}];
+var todos = [];
 
 app.get('/todos', function(req, res){
   res.send({todos: todos});
--- test/e2e/todoFunctionality.js
+++ test/e2e/todoFunctionality.js
@@ -1,8 +1,11 @@
 /* globals casper, document */
-casper.test.begin('App is setup correctly', 2, function suite(test) {
-  casper.start('http://localhost:3000/', function() {
-    test.assertExists('.todo-list', 'List should exist');
-    test.assertExists('.todo-form', 'Form should exist');
+
+var todoFormSelector = ".todo-form";
+
+casper.test.begin("App is setup correctly", 2, function suite(test) {
+  casper.start("http://localhost:3000/", function() {
+    test.assertExists(".todo-list", "List should exist");
+    test.assertExists(todoFormSelector, "Form should exist");
   });
 
   casper.run(function() {
@@ -10,4 +13,142 @@ casper.test.begin('App is setup correctly', 2, function suite(test) {
   });
 });
 
+casper.test.begin("Adds and removes todo items", 3, function suite(test) {
+  casper.start("http://localhost:3000/");
 
+  casper.waitForSelector(todoFormSelector, function() {
+    this.fill(
+      todoFormSelector,
+      {
+        todo: "Item1"
+      },
+      true
+    );
+
+    test.assertExists(
+      ".todo-list .todo-item",
+      "List item should exist after being added"
+    );
+
+    test.assertField(
+      { type: "css", path: ".todo-list .todo-item .todo-input" },
+      "Item1",
+      "List item should contain added item name"
+    );
+
+    this.click(".todo-remove");
+
+    test.assertDoesntExist(
+      ".todo-list .todo-item",
+      "List item should not exist after item removed"
+    );
+  });
+
+  casper.run(function() {
+    test.done();
+  });
+});
+
+casper.test.begin("Adds and removes multiple todo items", 3, function suite(
+  test
+) {
+  casper.start("http://localhost:3000/");
+
+  casper.waitForSelector(todoFormSelector, function() {
+    this.fill(
+      todoFormSelector,
+      {
+        todo: "Item1"
+      },
+      true
+    );
+
+    this.fill(
+      todoFormSelector,
+      {
+        todo: "Item2"
+      },
+      true
+    );
+
+    this.fill(
+      todoFormSelector,
+      {
+        todo: "Item3"
+      },
+      true
+    );
+
+    test.assertElementCount(
+      ".todo-list .todo-item",
+      3,
+      "3 items should be added"
+    );
+
+    test.assert(
+      casper.evaluate(function() {
+        var inputs = document.querySelectorAll(
+          ".todo-list .todo-item .todo-input"
+        );
+        return (
+          inputs[0].value === "Item1" &&
+          inputs[1].value === "Item2" &&
+          inputs[2].value === "Item3"
+        );
+      }),
+      "Items should contain added item names in correct order"
+    );
+
+    this.click(".todo-remove");
+    this.click(".todo-remove");
+    this.click(".todo-remove");
+
+    test.assertElementCount(
+      ".todo-list .todo-item",
+      0,
+      "0 items should remain after removal"
+    );
+  });
+
+  casper.run(function() {
+    test.done();
+  });
+});
+
+casper.test.begin("Marks todo items as done", 1, function suite(test) {
+  casper.start("http://localhost:3000/");
+
+  casper.waitForSelector(todoFormSelector, function() {
+    this.fill(
+      todoFormSelector,
+      {
+        todo: "Item1"
+      },
+      true
+    );
+
+    this.click(".todo-done");
+
+    test.assertExists(".todo-item--done", "List item should be marked as done");
+  });
+
+  casper.run(function() {
+    test.done();
+  });
+});
+
+casper.test.begin("Does not add empty todo items", 1, function suite(test) {
+  casper.start("http://localhost:3000/");
+
+  casper.waitForSelector(todoFormSelector, function() {
+    this.fill(todoFormSelector, {}, true);
+    test.assertDoesntExist(
+      ".todo-list .todo-item",
+      "List item should not be added for empty todos"
+    );
+  });
+
+  casper.run(function() {
+    test.done();
+  });
+});
