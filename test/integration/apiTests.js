c1cc74084eacd67c63719c259c1b7e1c401bd124
Quinton Aiken <quintonlaiken@gmail.com>
2019-03-28 11:11:05 -0700

    add integration tests

--- test/integration/apiTests.js
+++ test/integration/apiTests.js
@@ -1,6 +1,31 @@
-describe('API integration', function(){
+describe("API integration", function() {
   var server, setupStub, JSONresponse;
+  before(function() {
+    JSONresponse = {
+      todos: [
+        { name: "Test1", done: true },
+        { name: "Test2", done: false },
+        { name: "Test3", done: true }
+      ]
+    };
+    setupStub = sinon.stub(todo, "setup");
+    server = sinon.fakeServer.create();
+    server.respondWith("GET", "http://localhost:3000/todos", [
+      200,
+      { "Content-Type": "application/json" },
+      JSON.stringify(JSONresponse)
+    ]);
+  });
 
-  it('todo.setup receives an array of todos when todo.init is called', function () {
+  after(function() {
+    server.restore();
+    todo.setup.restore();
+  });
+
+  it("todo.setup receives an array of todos when todo.init is called", function() {
+    todo.init();
+    server.respond();
+    assert(setupStub.calledOnce);
+    assert(setupStub.calledWith(JSONresponse.todos));
   });
 });
